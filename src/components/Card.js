0fa3a0f556a92575512c182fcecf3de3b1228bac
srinivas.bh <srinivas.bh@agilophiles.com>
2023-08-07 14:48:03 +0530

    final commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Card = ({ item }) => {                                                                     
   const handleCardPress = () => {
-                                                                                            
+    console.log("Card pressed");                                                                                   
   };
 
   return (
@@ -37,9 +37,6 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    
-   
-    
   },
   image: {
     width: 100,
@@ -63,50 +60,3 @@ const styles = StyleSheet.create({
 });
 
 export default Card;
-
-
-{/* <View style={styles.cardContainer}>
-<Image
-  source={{
-    uri: `https://image.tmdb.org/t/p/original/${item.poster_path}`,
-  }}
-  style={styles.image}
-  resizeMode="cover"
-/>
-<View style={styles.detailsContainer}>
-  <Text style={styles.title}>{item.original_title}</Text>
-  <Text>{item.release_date}</Text>
-  <Text numberOfLines={2} ellipsizeMode="tail" style={styles.overview}>{item.overview}</Text>
-</View>
-</View> */}
-// cardContainer: {
-//   height: 384,
-//   width: 304,
-//   borderRadius: 12, 
-//   backgroundColor: "#ffffff",
-//   marginTop: 20,
-//   padding: 20,
-//   display: "flex",
-//   justifyContent: "center",
-//   alignItems: "center",
-// },
-// image: {
-//   width: 256,
-//   height: 173,
-//   borderRadius: 12,
-//   backgroundColor: "#DEDEDE",
-// },
-// detailsContainer: {
-//   width: 256,
-//   height: 81,
-//   marginTop: 20,
-//   padding: 20
-// },
-// title: {
-//   fontSize: 16,
-//   fontWeight: 'bold',
-//   marginBottom: 5,
-// },
-// overview: {
-//   overflow: "hidden"
-// }
